78e4144a883a1b18f5f5555e794743c7bf62710c
MattGrizzly <magrishams@gmail.com>
2022-06-02 16:52:48 -0500

    parties added to db

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql2');
 const express = require('express');
-const { CustomConsole } = require('@jest/console');
+const mysql = require('mysql2');
 const inputCheck = require('./utils/inputCheck');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,24 +11,24 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        // Your MySQL username,
-        user: 'root',
-        //Your MySQL password
-        password: '345ert#$%ERT',
-        database: 'election'
-    },
-    console.log('Connected to the election database.')
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
 );
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-  const sql = 'SELECT * FROM candidates';
+  const sql = `SELECT * FROM candidates`;
 
   db.query(sql, (err, rows) => {
     if (err) {
-      res.status(500).json({ error: err.message});
+      res.status(500).json({ error: err.message });
       return;
     }
     res.json({
@@ -37,17 +37,15 @@ app.get('/api/candidates', (req, res) => {
     });
   });
 });
-//db.query('SELECT * FROM candidates', (err, rows) => {
-//    console.log(rows);
-//});
-// GET a single candidate
+
+// Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = 'SELECT * FROM candidates WHERE id = ?';
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
     if (err) {
-      res.status(400).json({error:err.message});
+      res.status(400).json({ error: err.message });
       return;
     }
     res.json({
@@ -56,49 +54,58 @@ app.get('/api/candidate/:id', (req, res) => {
     });
   });
 });
+
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
-  const sql = 'DELETE FROM candidates WHERE id = ?';
+  const sql = `DELETE FROM candidates WHERE id = ?`;
   const params = [req.params.id];
 
-  db.query(sql, params, (err, reslut) => {
+  db.query(sql, params, (err, result) => {
     if (err) {
       res.statusMessage(400).json({ error: res.message });
-    } else if (!reslut.affectedRows) {
+    } else if (!result.affectedRows) {
       res.json({
         message: 'Candidate not found'
       });
-    }else {
+    } else {
       res.json({
         message: 'deleted',
-        changes: reslut.affectedRows,
+        changes: result.affectedRows,
         id: req.params.id
       });
     }
   });
 });
+
 // Create a candidate
-app.post('/api/candidate', ({body}, res) => {
-  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
   if (errors) {
     res.status(400).json({ error: errors });
     return;
   }
-  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-  VALUES (?,?,?)`;
-const params = [body.first_name, body.last_name, body.industry_connected];
 
-db.query(sql, params, (err, result) => {
-  if (err) {
-    res.status(400).json({ error: err.message });
-    return;
-  }
-  res.json({
-    message: 'success',
-    data: body
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
   });
 });
-});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
